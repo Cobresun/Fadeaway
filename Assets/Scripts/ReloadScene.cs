812620619f8c7eae3e1333014addcd687b1f8998
Brian Norman <briankn8@gmail.com>
2020-06-13 17:31:17 -0600

    Restart scene when zombie collide

---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TDGP.Demo
+{
+/// <summary>
+/// Reloads scene if enemy touches player in SampleScene.
+/// </summary>
+	public class ReloadScene : MonoBehaviour
+	{
+		void OnTriggerEnter2D (Collider2D other)
+		{
+			if (other.CompareTag ("Enemy"))
+				SceneManager.LoadScene ("SampleScene");
+		}
+	}
+}
+

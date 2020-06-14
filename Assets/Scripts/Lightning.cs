5daef417408d5b8b26b6b55bb7441bcf1303adc9
wesley-tang <wesleytang1@gmail.com>
2020-06-13 23:09:47 -0600

    Improve Lighting Logic

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -7,29 +7,39 @@ public class Lightning : MonoBehaviour
 {
 
     public Light2D pointLight; 
-    float tempTime;
+    float timeSinceFlash = 0;
+    float nextFlash;
+    float flashBuildUp = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        nextFlash = Random.Range(1, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        tempTime += Time.deltaTime;
-        if (pointLight.intensity == 0) {
-            if (tempTime > 0.8) {
-                pointLight.intensity = 1;
-                tempTime = 0;
+        timeSinceFlash += Time.deltaTime;
+
+        if (pointLight.intensity >= 0) {
+            pointLight.intensity -= 0.002f;
+            if (pointLight.intensity <= 0.6f) {
+                pointLight.intensity -= 0.001f;
+            }
+            else if (pointLight.intensity <= 1){
+                flashBuildUp += Time.deltaTime*Random.Range(0.07f, 0.02f);
+            }
+
+            if (flashBuildUp >= Random.Range(1.0f, 1.1f)){
+                pointLight.intensity += Random.Range(0.3f, 0.6f);
             }
         }
-        else {
-            if (tempTime > 1.8) {
-                pointLight.intensity = 0;
-                tempTime = 0;
-            }
+
+        if (timeSinceFlash >= nextFlash) {
+            pointLight.intensity = 1.5f;
+            timeSinceFlash = 0;
+            nextFlash = Random.Range(1.5f, 3.0f);
         }
     }
 }

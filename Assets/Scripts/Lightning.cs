13999023406c168f09b01cd7c824c8739d827048
Brian Norman <briankn8@gmail.com>
2020-06-13 20:29:28 -0600

    Make lighting flicker periodically

---
+++ Assets/Scripts/Lightning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Lightning : MonoBehaviour
+{
+
+    public Light2D pointLight; 
+    float tempTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempTime += Time.deltaTime;
+        if (tempTime > 1.75) {
+            if (pointLight.intensity == 0) {
+                pointLight.intensity = 1;
+            } else {
+                pointLight.intensity = 0;
+            }
+            tempTime = 0;
+        }
+    }
+}
